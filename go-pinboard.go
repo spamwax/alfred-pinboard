d25f9b6ade2f31cb823311a51c1a3698f34cdb8c
Michael Gruschwitz <info@grusch-it.de>
2014-08-18 22:01:30 +0200

    Add apple script to dynamically get the url and page title from the front most browser window
    supported browsers: Google Chrome, Safari, Firefox

--- go-pinboard.go
+++ go-pinboard.go
@@ -297,3 +297,47 @@ var appleScriptDetectBrowser = map[string]string{
             return {theURL, theDesc}
             end run`,
 }
+
+// get the url and title from the front most browser window (@see com.surrealroad.alfred-reminder by Jack James [http://www.surrealroad.com])
+var appleScriptGetUrlFromCurrentBrowser = `on run
+    set theApplication to (name of (info for (path to frontmost application)))
+    set theText to ""
+    set theURL to ""
+
+    if theApplication is "Google Chrome.app" then
+        tell application id "com.google.chrome"
+            using terms from application "Google Chrome"
+                set theText to title of active tab of first window
+                set theURL to get URL of active tab of first window
+            end using terms from
+        end tell
+    else if theApplication is "Safari.app" then
+        tell application id "com.apple.safari"
+            using terms from application "Safari"
+                set theTab to front document
+                set theText to name of theTab
+                set theURL to URL of theTab
+            end using terms from
+        end tell
+    else if theApplication is "Firefox.app" then
+        tell application "Firefox"
+            activate
+            set w to item 1 of window 1
+            set theDesc to name of w
+        end tell
+        tell application "System Events"
+            set myApp to name of first application process whose frontmost is true
+            if myApp is "Firefox" then
+                tell application "System Events"
+                    keystroke "l" using command down
+                    delay 0.5
+                    keystroke "c" using command down
+                end tell
+                delay 0.5
+            end if
+            delay 0.5
+        end tell
+        set theURL to get the clipboard
+    end if
+    return {theURL, theText}
+end run`
