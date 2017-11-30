2020a6b7eeb6b3757436c652cd3d963df9fe771c
Hamid Ghadyani <hamid@hamid.cc>
2017-11-29 19:37:33 -0500

    Space Linting changes.

--- search_bookmarks.go
+++ search_bookmarks.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "os"
-    "reflect"
-    "regexp"
-    "sort"
-    "strconv"
-    "strings"
-    "time"
+	"os"
+	"reflect"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
 
-    Alfred "bitbucket.org/listboss/go-alfred"
+	Alfred "bitbucket.org/listboss/go-alfred"
 )
 
 type bookmarkPair struct {
-    bookmark Link
-    date     time.Time
+	bookmark Link
+	date     time.Time
 }
 
 type sortedBookmarks []bookmarkPair
@@ -24,35 +24,36 @@ func (sb sortedBookmarks) Swap(i, j int)      { sb[i], sb[j] = sb[j], sb[i] }
 func (sb sortedBookmarks) Less(i, j int) bool { return sb[i].date.After(sb[j].date) }
 
 func showBookmarks(query []string, ga *Alfred.GoAlfred) {
-    err := getBookmarksContaining(query, ga)
-    if err != nil {
-        ga.MakeError(err)
-        ga.WriteToAlfred()
-        os.Exit(1)
-    }
-    ga.WriteToAlfred()
+	err := getBookmarksContaining(query, ga)
+	// os.Stdout.Write(err)
+	if err != nil {
+		ga.MakeError(err)
+		ga.WriteToAlfred()
+		os.Exit(1)
+	}
+	ga.WriteToAlfred()
 }
 
 func getBookmarksContaining(query []string, ga *Alfred.GoAlfred) (err error) {
-    var sPins sortedBookmarks
-    sPins, err = bookmarksContain(query, ga)
-    if err != nil {
-        return err
-    }
+	var sPins sortedBookmarks
+	sPins, err = bookmarksContain(query, ga)
+	if err != nil {
+		return err
+	}
 
-    ic := 0
-    for idx, v := range sPins {
-        pin := v.bookmark
-        icon := Alfred.NewIcon("pinboard-pin.icns", "")
-        ga.AddItem(strconv.Itoa(idx), pin.Desc, pin.Url, "yes", "", "", pin.Url, icon, false)
-        // ga.AddItem(uid, title, subtitle, valid, auto, rtype, arg, icon, check_valid)
+	ic := 0
+	for idx, v := range sPins {
+		pin := v.bookmark
+		icon := Alfred.NewIcon("pinboard-pin.icns", "")
+		ga.AddItem(strconv.Itoa(idx), pin.Desc, pin.Url, "yes", "", "", pin.Url, icon, false)
+		// ga.AddItem(uid, title, subtitle, valid, auto, rtype, arg, icon, check_valid)
 
-        ic++
-        if ic == MaxNoResults_Bookmarks {
-            break
-        }
-    }
-    return
+		ic++
+		if ic == MaxNoResults_Bookmarks {
+			break
+		}
+	}
+	return
 }
 
 // Workflow will search these 'attributes' of each bookmark
@@ -60,60 +61,60 @@ var PinAttrNames = []string{"Desc", "Tags", "Notes", "Url"}
 
 func bookmarksContain(query []string, ga *Alfred.GoAlfred) (sb sortedBookmarks, err error) {
 
-    posts := new(Posts)
-    if posts, err = readPostsCache(ga); err != nil {
-        return nil, err
-    }
+	posts := new(Posts)
+	if posts, err = readPostsCache(ga); err != nil {
+		return nil, err
+	}
 
-    // If fuzzy search is set, compile the corresponding regular expression.
-    var fuzzy, tag_only_search string
-    if fuzzy, err = ga.Get("fuzzy_search"); err != nil {
-        return nil, err
-    }
-    if tag_only_search, err = ga.Get("tag_only_search"); err != nil {
-        return nil, err
-    }
+	// If fuzzy search is set, compile the corresponding regular expression.
+	var fuzzy, tag_only_search string
+	if fuzzy, err = ga.Get("fuzzy_search"); err != nil {
+		return nil, err
+	}
+	if tag_only_search, err = ga.Get("tag_only_search"); err != nil {
+		return nil, err
+	}
 
-    var searchFields []string
-    if tag_only_search == "yes" {
-        searchFields = []string{"Tags"}
-    } else {
-        searchFields = PinAttrNames
-    }
+	var searchFields []string
+	if tag_only_search == "yes" {
+		searchFields = []string{"Tags"}
+	} else {
+		searchFields = PinAttrNames
+	}
 
-    var re *regexp.Regexp
+	var re *regexp.Regexp
 
-    for _, pin := range posts.Pins {
-        matchedPin := true
-        v := reflect.ValueOf(pin)
-        for _, q := range query {
-            q = strings.ToLower(q)
-            found_query := false
-            if fuzzy == "yes" {
-                re = buildRegExp(q)
-            }
+	for _, pin := range posts.Pins {
+		matchedPin := true
+		v := reflect.ValueOf(pin)
+		for _, q := range query {
+			q = strings.ToLower(q)
+			found_query := false
+			if fuzzy == "yes" {
+				re = buildRegExp(q)
+			}
 
-            for _, field_ := range searchFields {
-                fcontent := v.FieldByName(field_).String()
-                fcontent = strings.ToLower(fcontent)
-                if fuzzy == "yes" {
-                    found_query = re.MatchString(fcontent)
-                } else {
-                    found_query = strings.Contains(fcontent, q)
-                }
-                if found_query {
-                    break
-                }
-            }
-            if !found_query {
-                matchedPin = false
-                break
-            }
-        }
-        if matchedPin {
-            sb = append(sb, bookmarkPair{bookmark: pin, date: pin.Time})
-        }
-    }
-    sort.Sort(sb)
-    return sb, err
+			for _, field_ := range searchFields {
+				fcontent := v.FieldByName(field_).String()
+				fcontent = strings.ToLower(fcontent)
+				if fuzzy == "yes" {
+					found_query = re.MatchString(fcontent)
+				} else {
+					found_query = strings.Contains(fcontent, q)
+				}
+				if found_query {
+					break
+				}
+			}
+			if !found_query {
+				matchedPin = false
+				break
+			}
+		}
+		if matchedPin {
+			sb = append(sb, bookmarkPair{bookmark: pin, date: pin.Time})
+		}
+	}
+	sort.Sort(sb)
+	return sb, err
 }
--- update_cache.go
+++ update_cache.go
@@ -1,266 +1,266 @@
 package main
 
 import (
-    "bufio"
-    "bytes"
-    "encoding/gob"
-    "encoding/xml"
-    "errors"
-    "io/ioutil"
-    "net/http"
-    "net/url"
-    "os"
-    "strings"
-    "time"
+	"bufio"
+	"bytes"
+	"encoding/gob"
+	"encoding/xml"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 
-    Alfred "bitbucket.org/listboss/go-alfred"
+	Alfred "bitbucket.org/listboss/go-alfred"
 )
 
 type pinboardUpdateResponse struct {
-    XMLName  xml.Name  `xml:"update"`
-    Datetime time.Time `xml:"time,attr"`
+	XMLName  xml.Name  `xml:"update"`
+	Datetime time.Time `xml:"time,attr"`
 }
 
 type pinboardResultResponse struct {
-    XMLName xml.Name `xml:"result"`
-    Code    string   `xml:"code,attr"`
+	XMLName xml.Name `xml:"result"`
+	Code    string   `xml:"code,attr"`
 }
 
 type Posts struct {
-    XMLName xml.Name `xml:"posts"`
-    Pins    []Link   `xml:"post"`
+	XMLName xml.Name `xml:"posts"`
+	Pins    []Link   `xml:"post"`
 }
 
 type Link struct {
-    XMLName xml.Name  `xml:"post"`
-    Url     string    `xml:"href,attr"`
-    Desc    string    `xml:"description,attr"`
-    Notes   string    `xml:"extended,attr"`
-    Time    time.Time `xml:"time,attr"`
-    Hash    string    `xml:"hash,attr"`
-    Shared  bool      `xml:"shared,attr"`
-    Tags    string    `xml:"tag,attr"`
-    Meta    string    `xml:"meta,attr"`
+	XMLName xml.Name  `xml:"post"`
+	Url     string    `xml:"href,attr"`
+	Desc    string    `xml:"description,attr"`
+	Notes   string    `xml:"extended,attr"`
+	Time    time.Time `xml:"time,attr"`
+	Hash    string    `xml:"hash,attr"`
+	Shared  bool      `xml:"shared,attr"`
+	Tags    string    `xml:"tag,attr"`
+	Meta    string    `xml:"meta,attr"`
 }
 
 type Tags map[string]uint
 
 func update_tags_cache(ga *Alfred.GoAlfred) (err error) {
-    needed, err := updateNeeded(ga)
-    if err != nil {
-        return err
-    }
-    if !needed {
-        return err
-    }
+	needed, err := updateNeeded(ga)
+	if err != nil {
+		return err
+	}
+	if !needed {
+		return err
+	}
 
-    if err = updatePostsCache(ga); err != nil {
-        return err
-    }
+	if err = updatePostsCache(ga); err != nil {
+		return err
+	}
 
-    posts := new(Posts)
-    if posts, err = readPostsCache(ga); err != nil {
-        return err
-    }
+	posts := new(Posts)
+	if posts, err = readPostsCache(ga); err != nil {
+		return err
+	}
 
-    tags_map := make(Tags)
-    for _, pin := range posts.Pins {
-        tags := strings.Fields(pin.Tags)
-        if len(tags) != 0 {
-            for _, tag := range tags {
-                count := tags_map[tag]
-                count++
-                tags_map[tag] = count
-            }
-        }
-    }
+	tags_map := make(Tags)
+	for _, pin := range posts.Pins {
+		tags := strings.Fields(pin.Tags)
+		if len(tags) != 0 {
+			for _, tag := range tags {
+				count := tags_map[tag]
+				count++
+				tags_map[tag] = count
+			}
+		}
+	}
 
-    tags_cache_fn, err := ga.Get("tags_cache_fn")
-    if err != nil {
-        return err
-    }
-    tags_map.store_tags_cache(tags_cache_fn)
-    if err != nil {
-        return err
-    }
+	tags_cache_fn, err := ga.Get("tags_cache_fn")
+	if err != nil {
+		return err
+	}
+	tags_map.store_tags_cache(tags_cache_fn)
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 func updatePostsCache(ga *Alfred.GoAlfred) error {
-    u, err := makeURLWithAuth(ga, "v1/posts/all")
-    if err != nil {
-        return err
-    }
+	u, err := makeURLWithAuth(ga, "v1/posts/all")
+	if err != nil {
+		return err
+	}
 
-    res, err := fetchDataFromHttp(u)
-    if err != nil {
-        return err
-    }
+	res, err := fetchDataFromHttp(u)
+	if err != nil {
+		return err
+	}
 
-    fn, err := ga.Get("posts_cache_fn")
-    if err != nil {
-        return err
-    }
-    file, err := os.Create(fn)
-    defer file.Close()
-    if err != nil {
-        return err
-    }
-    n, err := file.WriteString(string(res))
-    if n != len(res) {
-        return errors.New("Wrote different # of bytes than expected.")
-    }
-    ga.Set("update_time", time.Now().Format(time.RFC3339Nano))
-    return nil
+	fn, err := ga.Get("posts_cache_fn")
+	if err != nil {
+		return err
+	}
+	file, err := os.Create(fn)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	n, err := file.WriteString(string(res))
+	if n != len(res) {
+		return errors.New("Wrote different # of bytes than expected.")
+	}
+	ga.Set("update_time", time.Now().Format(time.RFC3339Nano))
+	return nil
 }
 
 func readPostsCache(ga *Alfred.GoAlfred) (posts *Posts, err error) {
-    posts = new(Posts)
+	posts = new(Posts)
 
-    fn, err := ga.Get("posts_cache_fn")
-    if err != nil {
-        return nil, err
-    }
-    file, err := os.Open(fn)
-    defer file.Close()
-    if err != nil {
-        return nil, err
-    }
-    result, err := ioutil.ReadAll(file)
-    if err != nil {
-        return nil, err
-    }
-    rd := strings.NewReader(string(result))
-    brd := bufio.NewReaderSize(rd, rd.Len())
-    p := make([]byte, rd.Len())
-    brd.Read(p)
-    if err = xml.Unmarshal(p, posts); err != nil {
-        return nil, err
-    }
-    return posts, nil
+	fn, err := ga.Get("posts_cache_fn")
+	if err != nil {
+		return nil, err
+	}
+	file, err := os.Open(fn)
+	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
+	result, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	rd := strings.NewReader(string(result))
+	brd := bufio.NewReaderSize(rd, rd.Len())
+	p := make([]byte, rd.Len())
+	brd.Read(p)
+	if err = xml.Unmarshal(p, posts); err != nil {
+		return nil, err
+	}
+	return posts, nil
 }
 
 func updateNeeded(ga *Alfred.GoAlfred) (flag bool, err error) {
-    u, err := makeURLWithAuth(ga, "/v1/posts/update")
-    if err != nil {
-        return false, err
-    }
+	u, err := makeURLWithAuth(ga, "/v1/posts/update")
+	if err != nil {
+		return false, err
+	}
 
-    status, err := fetchDataFromHttp(u)
-    if err != nil {
-        return false, err
-    }
+	status, err := fetchDataFromHttp(u)
+	if err != nil {
+		return false, err
+	}
 
-    var pinRes pinboardUpdateResponse
-    if err = xml.Unmarshal(status, &pinRes); err != nil {
-        return false, err
-    }
+	var pinRes pinboardUpdateResponse
+	if err = xml.Unmarshal(status, &pinRes); err != nil {
+		return false, err
+	}
 
-    // Get the time/date that the cache was last updated
-    var lastTime time.Time
-    var last_update string
-    if last_update, err = ga.Get("update_time"); err != nil {
-        return false, err
-    }
-    if last_update == "" { // No update has yet been made
-        last_update = "0001-01-01T23:00:00Z"
-    }
+	// Get the time/date that the cache was last updated
+	var lastTime time.Time
+	var last_update string
+	if last_update, err = ga.Get("update_time"); err != nil {
+		return false, err
+	}
+	if last_update == "" { // No update has yet been made
+		last_update = "0001-01-01T23:00:00Z"
+	}
 
-    // missing tags cache file
-    tags_cache_fn, err := ga.Get("tags_cache_fn")
-    if err != nil {
-        return false, err
-    }
-    if _, err := os.Stat(tags_cache_fn); os.IsNotExist(err) {
-        return true, nil
-    }
+	// missing tags cache file
+	tags_cache_fn, err := ga.Get("tags_cache_fn")
+	if err != nil {
+		return false, err
+	}
+	if _, err := os.Stat(tags_cache_fn); os.IsNotExist(err) {
+		return true, nil
+	}
 
-    // missing posts cache file
-    posts_cache_fn, err := ga.Get("posts_cache_fn")
-    if err != nil {
-        return false, err
-    }
-    if _, err := os.Stat(posts_cache_fn); os.IsNotExist(err) {
-        return true, nil
-    }
+	// missing posts cache file
+	posts_cache_fn, err := ga.Get("posts_cache_fn")
+	if err != nil {
+		return false, err
+	}
+	if _, err := os.Stat(posts_cache_fn); os.IsNotExist(err) {
+		return true, nil
+	}
 
-    if lastTime, err = time.Parse(time.RFC3339Nano, last_update); err != nil {
-        return false, err
-    }
+	if lastTime, err = time.Parse(time.RFC3339Nano, last_update); err != nil {
+		return false, err
+	}
 
-    if pinRes.Datetime.After(lastTime) {
-        return true, nil
-    } else {
-        return false, nil
-    }
-    return
+	if pinRes.Datetime.After(lastTime) {
+		return true, nil
+	} else {
+		return false, nil
+	}
+	return
 }
 
 func fetchDataFromHttp(u url.URL) ([]byte, error) {
-    res, err := http.Get(u.String())
-    if err != nil {
-        return nil, err
-    }
-    if res.StatusCode != http.StatusOK {
-        return nil, errors.New(res.Status)
-    }
-    result, err := ioutil.ReadAll(res.Body)
-    res.Body.Close()
-    if err != nil {
-        return nil, err
-    }
-    return result, nil
+	res, err := http.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New(res.Status)
+	}
+	result, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
 func makeURLWithAuth(ga *Alfred.GoAlfred, pathURL string) (url.URL, error) {
-    u := url.URL{}
-    u.Scheme = hostURLScheme
-    u.Host = hostURLPinboard  // path.Join(hostURLPinboard, pathURL)
-    u.Path = pathURL
-    q := u.Query()
+	u := url.URL{}
+	u.Scheme = hostURLScheme
+	u.Host = hostURLPinboard // path.Join(hostURLPinboard, pathURL)
+	u.Path = pathURL
+	q := u.Query()
 
-    auth_token, err := ga.Get("oauth")
-    if err != nil {
-        return url.URL{}, err
-    } else if auth_token == "" {
-        return url.URL{}, errors.New("Set your authorization token first!")
-    }
+	auth_token, err := ga.Get("oauth")
+	if err != nil {
+		return url.URL{}, err
+	} else if auth_token == "" {
+		return url.URL{}, errors.New("Set your authorization token first!")
+	}
 
-    q.Set("auth_token", auth_token)
-    u.RawQuery = q.Encode()
-    return u, nil
+	q.Set("auth_token", auth_token)
+	u.RawQuery = q.Encode()
+	return u, nil
 }
 
 func (tm *Tags) store_tags_cache(fn string) error {
-    file, err := os.Create(fn)
-    defer file.Close()
-    if err != nil {
-        return err
-    }
+	file, err := os.Create(fn)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
 
-    var b bytes.Buffer
-    enc := gob.NewEncoder(&b)
-    if err = enc.Encode(tm); err != nil {
-        return err
-    }
+	var b bytes.Buffer
+	enc := gob.NewEncoder(&b)
+	if err = enc.Encode(tm); err != nil {
+		return err
+	}
 
-    file.Write(b.Bytes())
-    return nil
+	file.Write(b.Bytes())
+	return nil
 }
 
 func load_tags_cache(fn string) (Tags, error) {
-    file, err := os.Open(fn)
-    defer file.Close()
-    if err != nil {
-        return nil, err
-    }
+	file, err := os.Open(fn)
+	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
 
-    tags_map := make(Tags)
-    decoder := gob.NewDecoder(file)
-    if err = decoder.Decode(&tags_map); err != nil {
-        return nil, err
-    }
+	tags_map := make(Tags)
+	decoder := gob.NewDecoder(file)
+	if err = decoder.Decode(&tags_map); err != nil {
+		return nil, err
+	}
 
-    return tags_map, nil
+	return tags_map, nil
 }
